fc96c6cb1e9ecbf7b34011e60c9299492fef0119
Gorschenko <aa.gorschenko@yandex.ru>
2022-08-29 20:53:59 +0300

    Test file for edition was added

--- test.ts
+++ test.ts
@@ -48,6 +48,8 @@
 // 	}
 // }
 
+// ИНТЕРФЕЙСЫ И ТИПЫ
+
 // type coord = { lat: number, long: number }
 
 // interface ICoord {
@@ -100,3 +102,143 @@
 // 	name: 'sds',
 // 	tail: true,
 // }
+
+// ИНТЕРФЕЙСЫ И ТИПЫ
+
+// ЛИТЕРАЛЫ
+// let b: 'hi' = 'hi' // - литерал
+
+// type direction = 'left' | 'right'
+
+// function moveDog(direction: direction): -1 | 0 | 1 {
+// 	switch(direction) {
+// 		case 'left':
+// 			return -1
+// 		case 'right':
+// 			return 1
+// 		default:
+// 			return 0
+// 	}
+// }
+
+// interface IConnection {
+// 	host: string,
+// 	port: number
+// }
+// function connect(connection: IConnection | 'default') {
+
+// }
+
+// connect()
+
+// const connection = {
+// 	host: 'localhost',
+// 	protocol: 'https' as 'https', // Задаем ключу значение литерала
+// }
+
+// let a: any = 5
+// let c = a as number
+
+// function connect (host: string, protocol: 'http' | 'https') {
+
+// }
+// connect (connection.host, connection.protocol) 
+
+// ЛИТЕРАЛЫ
+
+// ЕНАМЫ
+// type direction = 'left' | 'right'
+
+// enum Direction {
+// 	Left,
+// 	Right
+// }
+
+// function move(direction: Direction) {
+// 	switch (direction) {
+// 		case Direction.Left:
+// 			return -1
+// 		case Direction.Right:
+// 			return 1
+// 	}
+// }
+
+// function objMod(obj: { Left: number }) {
+
+// }
+
+// objMod(Direction)
+
+// const enum Direction2 {
+// 	Up,
+// 	Down
+// }
+
+// let myDirection = Direction2.Up
+
+// ЕНАМЫ
+
+// ДЖЕНЕРИКИ - позволяют не дублировать функцию и
+// работать с разными типами аргументов, которые передаем
+
+// function log(obj: string): string {
+// 	console.log(obj)
+// 	return obj
+// }
+
+// function log2(obj: number): number {
+// 	console.log(obj)
+// 	return obj
+// }
+
+// interface HasLength {
+// 	length: number
+// }
+
+// function log<T extends HasLength, K>(obj: T, arr: K[]): K[] {
+// 	console.log(obj)
+// 	return arr
+// }
+
+// interface IUser {
+// 	name: string,
+// 	age?: number,
+// 	bid: <T>(sum: T) => boolean,
+// }
+
+// function bid<T>(sum: T): boolean {
+// 	return true
+// }
+
+// ДЖЕНЕРИКИ
+
+// КЛАССЫ
+// interface LoggerService {
+// 	log: (s: string) => void
+// }
+
+// class Logger implements LoggerService { // заставляет имплементировать (повторить) логику интерфейса
+// 	log(s: string) {
+// 		console.log(s)
+// 	}
+// }
+
+// class MyClass<T> {
+// 	a: <T>
+// }
+
+// const b = new MyClass<string>()
+// b.a
+
+// КЛАССЫ
+
+// ДРУГОЕ
+type Coord = {
+	lat: number,
+	long: number
+}
+type P = keyof Coord
+
+let a: P = 'long'
+
+// ДРУГОЕ
