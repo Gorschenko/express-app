0aa3aad91ffd1903c64887e150e1f5e0d95feb4a
Alexey <aa.gorschenko@yandex.ru>
2022-10-02 13:19:43 +0300

    DTO and user entity were added

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { ExeptionFilter } from './errors/exeption.filter'
 import { inject, injectable } from 'inversify'
 import { TYPES } from './types'
 import 'reflect-metadata'
+import { json } from 'body-parser'
 
 @injectable()
 export class App {
@@ -22,14 +23,20 @@ export class App {
 		this.port = 8000
 	}
 
+	useMiddleware(): void {
+		this.app.use(json())
+	}
+
 	useRoutes(): void {
 		this.app.use('/users', this.usersController.router)
 	}
+
 	useExeptionFilters(): void {
 		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
 	}
 
 	public async init(): Promise<void> {
+		this.useMiddleware()
 		this.useRoutes()
 		this.useExeptionFilters()
 		this.server = this.app.listen(this.port)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,9 @@ import { Request, Response, NextFunction } from 'express'
 import { ILogger } from '../logger/logger.interface'
 import 'reflect-metadata'
 import { IUserController } from './users.controller.interface'
+import { UserLoginDto } from './dto/user-login.dto'
+import { UserRegisterDto } from './dto/user-register.dto'
+import { User } from './user.entity'
 @injectable()
 export class UsersController extends BaseController implements IUserController {
 	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
@@ -16,10 +19,16 @@ export class UsersController extends BaseController implements IUserController {
 		])
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
 		next(new HTTPError(401, 'Ошибка авторизации'))
 	}
-	register(req: Request, res: Response, next: NextFunction): void {
-		this.ok(res, 'register')
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const newUser = new User(body.email, body.name)
+		await newUser.setPassword(body.password)
+		this.ok(res, newUser)
 	}
 }
